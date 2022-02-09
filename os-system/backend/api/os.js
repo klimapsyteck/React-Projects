17485b66140057362eabd6af18fd4b2463c93b44
Kesley Lima <kesleylimasi@gmail.com>
2022-02-09 14:06:42 -0300

    going

---
+++ os-system/backend/api/os.js
@@ -0,0 +1,20 @@
+
+module.exports = app =>{
+    const save = (req, res) => {
+
+    }
+
+    const getAll = (req, res) => {
+
+    }
+
+    const getById = (req, res) => {
+
+    }
+
+    const remove = (req, res) => {
+
+    }
+
+    return {save, getAll, getById, remove}
+}
